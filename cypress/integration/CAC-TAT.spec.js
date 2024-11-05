da830f72e2c383450e2cc1aec1fdf052557a6237
Khazir <danieltr6@gmail.com>
2024-11-04 23:53:26 -0300

    Cypress Basico Comcluído

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,4 +1,6 @@
 describe('Central de Atendimento ao Cliente TAT', () =>  {
+    const threesc = 3000
+
  beforeEach(() => {
   cy.visit('./src/index.html')
     })
@@ -8,14 +10,18 @@ it('Verification of title', () => {
       .should('be.equal', 'Central de Atendimento ao Cliente TAT')
 })
 it('Asserting that the text fields are ready for typing and click button sends request', () => {
-    const test = 'Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma de, '
+    const test = Cypress._.repeat('uma forma de', 20)
+
+    cy.clock()
 
     cy.get('#firstName').should('be.visible').type('Daniel').should('have.value', 'Daniel')
     cy.get('#lastName').should('be.visible').type('Tiburcio').should('have.value', 'Tiburcio')
     cy.get('#email').should('be.visible').type('daniel@gmail.com').should('have.value', 'daniel@gmail.com')
-    cy.get('#open-text-area').should('be.visible').type(test, {delay:0})
+    cy.get('#open-text-area').should('be.visible').invoke('val',test).should('have.value', test)
     cy.contains('button', 'Enviar').should('be.visible').click()
     cy.get('.success').should('be.visible')
+    cy.tick (threesc)
+    cy.get('.success').should('not.be.visible')
 
      //###Bloco para testar digitação errada de email###\\
      cy.get('#firstName').should('be.visible').type('Daniel').should('have.value', 'Daniel')
@@ -32,13 +38,13 @@ it('Checking if the phone number fields are being entered correctly', () =>{
     })
 
 it('Checking if the telephone field checkbox is marked, filling it out becomes mandatory.', () =>{
-const test = 'Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma de, '
+const test = Cypress._.repeat('uma forma de', 20)
 
     cy.get('#firstName').should('be.visible').type('Daniel').should('have.value', 'Daniel')
     cy.get('#lastName').should('be.visible').type('Tiburcio').should('have.value', 'Tiburcio')
     cy.get('#email').should('be.visible').type('daniel@gmail.com').should('have.value', 'daniel@gmail.com')
     cy.get('#phone-checkbox').check()
-    cy.get('#open-text-area').should('be.visible').type(test, {delay:0})
+    cy.get('#open-text-area').should('be.visible').invoke('val', test).should('have.value', test)
     cy.contains('button', 'Enviar').should('be.visible').click()
     cy.get('.error').should('be.visible') //erro ao não digitar o numero de telefone obrigatório
     cy.get('#phone').type('932223333') // digitação do numero
@@ -48,12 +54,12 @@ const test = 'Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma 
 
 it('Cleaning fields test check', () =>{
     //digitando, limpando e verificando os valores nos campos
-    const test = 'Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma de, Uma forma de, '
+    const test = Cypress._.repeat('uma forma de', 20)
 
     cy.get('#firstName').should('be.visible').type('Daniel').should('have.value', 'Daniel').clear().should('have.value', '')
     cy.get('#lastName').should('be.visible').type('Tiburcio').should('have.value', 'Tiburcio').clear().should('have.value', '')
     cy.get('#email').should('be.visible').type('daniel@gmail.com').should('have.value', 'daniel@gmail.com').clear().should('have.value', '')
-    cy.get('#open-text-area').should('be.visible').type(test, {delay:0}).should('have.value',test).clear().should('have.value', '')
+    cy.get('#open-text-area').should('be.visible').invoke('val', test).should('have.value', test).clear().should('have.value', '')
 
     })
 
@@ -126,10 +132,45 @@ expect($input[0].files[0].name).to.equal('example.json')
 
  })  
  
- it('Opening the privacy policy tab in the same tab as the home page (removing the target attribute)', () => {
+ /*it('Opening the privacy policy tab in the same tab as the home page (removing the target attribute)', () => {
     cy.get('#privacy a').invoke('removeAttr', 'target').click()
     cy.contains('p', 'Talking About Testing').should('be.visible')
 
- })
+ })*/
+
+it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+        cy.get('.success')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Mensagem enviada com sucesso.')
+          .invoke('hide')
+          .should('not.be.visible')
+        cy.get('.error')
+          .should('not.be.visible')
+          .invoke('show')
+          .should('be.visible')
+          .and('contain', 'Valide os campos obrigatórios!')
+          .invoke('hide')
+          .should('not.be.visible')
+      })
+
+      it('Make a http request', () => {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+        .should(function(response) {
+            const {status, statusText, body} = response
+            expect(status).to.equal(200)
+            expect(statusText).to.equal('OK')
+            expect(body).to.include('CAC TAT')
+        })
+
+
+      })
+
+      it.only('Found the hiden cat', () =>{
+        cy.get('#cat').invoke('show').should('be.visible').invoke('hide').should('not.be.visible')
+      })
+
+      
 
  })
--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,4 +1,7 @@
-it.only('Testing only de privacy policy page',() =>{
-cy.visit('./src/privacy.html')
-cy.contains('#title', 'CAC TAT - Política de privacidade').should('be.visible')
+Cypress._.times(3, () => {
+    it('Testing only de privacy policy page',() =>{
+        cy.visit('./src/privacy.html')
+        cy.contains('#title', 'CAC TAT - Política de privacidade').should('be.visible')
+        })
+
 })
